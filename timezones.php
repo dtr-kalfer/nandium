5598efb336efb8cada05ac376bc349df6060a659
Ferdinand Tumulak <mymikrotiki@gmail.com>
2026-06-24 23:55:31 +0800

    added: timezones.php, various timezones

---
+++ timezones.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'Asia/Manila', 'Asia/Singapore', 'Asia/Hong_Kong', 'Asia/Tokyo', 'Asia/Seoul', 'Asia/Bangkok', 'Asia/Dubai',
+    'UTC', 'GMT',
+    'America/New_York', 'America/Chicago', 'America/Denver', 'America/Los_Angeles',
+    'Europe/London', 'Europe/Paris', 'Europe/Berlin',
+    'Australia/Sydney', 'Australia/Melbourne',
+];
