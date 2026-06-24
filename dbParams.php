799c2c13fa529bbfcaffb21c5b1ac11e98cedfdb
Ferdinand Tumulak <mymikrotiki@gmail.com>
2026-06-24 23:29:45 +0800

    added: dbParams.php, this php code allows CRUD communication between database and backend/frontend interaction.

---
+++ dbParams.php
@@ -0,0 +1,10 @@
+<?php
+
+// Database connection parameters for WAMP setup
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'weather_feedback'); // You can name your database this
+define('DB_USER', 'weather_feedback_user'); // Default WAMP user
+define('DB_PASS', 'default_password_12345'); // Default WAMP password is empty
+
+?>
